c6f6194c30dac47e26a4df4fdc0e60cd7c1f8043
TheHajuenter <esjeruk517@gmail.com>
2025-06-21 12:53:17 +0700

    add blog card and blog list component to home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Blog from "./pages/Blog";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/" element={<Blog />} />
       </Routes>
     </div>
   );
