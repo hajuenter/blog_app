e90215d57bf63da164e49d5e71a0502b019e27b5
TheHajuenter <esjeruk517@gmail.com>
2025-06-22 09:52:56 +0700

    add structure admin page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
+import Layout from "./pages/admin/Layout";
+import Dashboard from "./pages/admin/Dashboard";
+import AddBlog from "./pages/admin/AddBlog";
+import ListBlog from "./pages/admin/ListBlog";
+import Comment from "./pages/admin/Comment";
 
 const App = () => {
   return (
@@ -9,6 +14,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/blog/:id" element={<Blog />} />
+        <Routes path="/admin" element={<Layout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="addBlog" element={<AddBlog />} />
+          <Route path="listBlog" element={<ListBlog />} />
+          <Route path="comment" element={<Comment />} />
+        </Routes>
       </Routes>
     </div>
   );
