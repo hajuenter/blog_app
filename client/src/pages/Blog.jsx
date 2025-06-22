dfc1e2e087a9e558b130f243c3065746f34a89b8
TheHajuenter <esjeruk517@gmail.com>
2025-06-22 17:39:08 +0700

    fixed comment massage and date in mobile view in blog page

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -69,7 +69,7 @@ const Blog = () => {
             {comment.map((item, index) => (
               <div
                 key={index}
-                className="relative bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600"
+                className="bg-primary/2 border border-primary/5 max-w-xl p-4 rounded text-gray-600"
               >
                 <div className="flex items-center gap-2 mb-2">
                   <img
@@ -79,8 +79,10 @@ const Blog = () => {
                   />
                   <p className="font-medium">{item.name}</p>
                 </div>
-                <p className="text-sm max-w-md ml-8">{item.content}</p>
-                <div className="absolute right-4 bottom-3 flex items-center gap-2 text-xs">
+
+                <p className="text-sm ml-8 mb-2">{item.content}</p>
+
+                <div className="flex justify-end mt-2 text-xs text-gray-400">
                   {Moment(item.createdAt).fromNow()}
                 </div>
               </div>
